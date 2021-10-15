2d3109f1a01120f55b13238fecc444d936b08f3f
okabe; <irvaniali79@gmail.com>
2021-10-15 17:42:18 +0330

    cors,kernel,bootstrap.js configuration for sanctum installation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,6 +10,7 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+axios.defaults.withCredentials = true;
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
@@ -26,3 +27,4 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
