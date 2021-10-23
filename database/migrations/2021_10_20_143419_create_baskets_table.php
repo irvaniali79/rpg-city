151123902eb43701a58b82ac92edf78a1aa558c1
okabe; <irvaniali79@gmail.com>
2021-10-23 09:53:00 +0330

    define migrations for article_basket relation

--- database/migrations/2021_10_20_143419_create_baskets_table.php
+++ database/migrations/2021_10_20_143419_create_baskets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateBasketsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('baskets', function (Blueprint $table) {
             $table->id();
-            
-            $table->string('name');
-            $table->integer('entity');
-            $table->float('price');
-
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
@@ -31,6 +27,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('baskets');
     }
 }
--- database/migrations/2021_10_20_150040_create_article_basket_table.php
+++ database/migrations/2021_10_20_150040_create_article_basket_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArticlesTable extends Migration
+class CreateArticleBasketTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateArticlesTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('article_basket', function (Blueprint $table) {
             $table->id();
-            
-            $table->string('name');
-            $table->integer('entity');
-            $table->float('price');
-
+            $table->foreignId('article_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('basket_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateArticlesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('articles');
+        Schema::dropIfExists('article_basket');
     }
 }
