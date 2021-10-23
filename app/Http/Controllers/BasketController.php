3d0dca0b6e96c93dbed7fc28bcfdc4342c105dbd
okabe; <irvaniali79@gmail.com>
2021-10-23 09:50:44 +0330

    inital make Basket/favorite Controller

---
+++ app/Http/Controllers/BasketController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Basket;
+use Illuminate\Http\Request;
+
+class BasketController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Basket $basket)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Basket  $basket
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Basket $basket)
+    {
+        //
+    }
+}
