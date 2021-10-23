3949361074b54fe1e313218436e12db2be0cbaf3
okabe; <irvaniali79@gmail.com>
2021-10-23 09:44:51 +0330

    build and define relation basket_articles

---
+++ app/Models/Basket.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Basket extends Model
+{
+    use HasFactory;
+    
+    
+    function articles() {
+        return $this->belongsToMany(Article::class);
+    }
+    
+}
