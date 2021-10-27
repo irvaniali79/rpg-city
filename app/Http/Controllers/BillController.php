1950abbeac0ff2f9d5338b7b64abb009bc1823b0
okabe; <irvaniali79@gmail.com>
2021-10-27 10:33:51 +0330

    bill.index imple

---
+++ app/Http/Controllers/BillController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bill;
+use Illuminate\Http\Request;
+
+class BillController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        return response([
+            'Data'=>Bill::orderBy('created_at','asc')->get()->load('user')
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Bill  $bill
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Bill $bill)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Bill  $bill
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Bill $bill)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Bill  $bill
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Bill $bill)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Bill  $bill
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Bill $bill)
+    {
+        //
+    }
+}
