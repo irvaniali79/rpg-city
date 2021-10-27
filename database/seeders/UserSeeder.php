59a407c78ab24fcc0c9330b7da499dcc1e036ecc
okabe; <irvaniali79@gmail.com>
2021-10-27 10:37:42 +0330

    adding Bill maker to UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,10 +3,12 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
 use App\Models\Article;
 use App\Models\Basket;
 use App\Models\Favorite;
+use App\Models\Bill;
 
 class UserSeeder extends Seeder
 {
@@ -18,8 +20,13 @@ class UserSeeder extends Seeder
     public function run()
     {
        
-        User::factory(5)->has(Basket::factory(1)->has(Article::factory(5)))
-        ->has(Favorite::factory(1)->has(Article::factory(5)))->create();
+       User::factory(5)->has(Basket::factory(1)->has(Article::factory(5)))
+        ->has(Favorite::factory(1)->has(Article::factory(5)))->
+       has(Bill::factory(3)->has(Basket::factory(1)))->create();
+       
         
+
+        
+      
     }
 }
