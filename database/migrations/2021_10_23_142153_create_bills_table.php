048adc3e36f19594d9e779c3758d320220495525
okabe; <irvaniali79@gmail.com>
2021-10-27 10:32:25 +0330

    bill migration inital imple

---
+++ database/migrations/2021_10_23_142153_create_bills_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bills', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('user_id')->constrained();
+            
+            $table->enum('status',['queue','grouping','load','complete'])->default('queue');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bills');
+    }
+}
