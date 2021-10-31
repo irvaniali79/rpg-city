a4d58c48536567b58450019b8de6640aea70567c
okabe; <irvaniali79@gmail.com>
2021-10-31 17:30:10 +0330

    art make:model Media -rmf

---
+++ app/Models/Media.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    use HasFactory;
+    
+    protected $fillable=[
+        'path'
+    ];
+    
+    
+    function article() {
+        return $this->belongsTo(Article::class);
+    }
+}
