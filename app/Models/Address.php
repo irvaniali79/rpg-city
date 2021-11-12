4e89daa8acec171ce53604b116c68d43285fa6bf
okabe; <irvaniali79@gmail.com>
2021-11-12 14:27:13 +0330

    edit address fillable array

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -10,7 +10,7 @@ class Address extends Model
     use HasFactory;
     protected $fillable=[
         'address',
-        'discription',
+        'description',
         'name',
         'region',
         'city',
