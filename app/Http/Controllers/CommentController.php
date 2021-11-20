4a8b38bcf9c4f19c630e09d42f54d56f7862fcd2
okabe; <irvaniali79@gmail.com>
2021-11-20 13:34:17 +0330

    costumize commentController for rest applations

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,87 +2,146 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Comment;
-use App\Models\User;
-use Illuminate\Validation\Rule;
-use Illuminate\Auth\Events\Validated;
 
-class CommentController extends Controller
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Honeypot\ProtectAgainstSpam;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Validator;
+use Laravelista\Comments\CommentControllerInterface;
+use Laravelista\Comments\Comment;
+
+class CommentController extends Controller implements CommentControllerInterface
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function __construct()
     {
-        return response([
-            'Data'=>Comment::all()
-        ]);
+        $this->middleware('api');
+        
+        if (Config::get('comments.guest_commenting') == true) {
+            $this->middleware('auth')->except('store');
+            $this->middleware(ProtectAgainstSpam::class)->only('store');
+        } else {
+            $this->middleware('auth');
+        }
     }
     
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-    
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * Creates a new comment for given model.
      */
     public function store(Request $request)
     {
-        //
+        // If guest commenting is turned off, authorize this action.
+        if (Config::get('comments.guest_commenting') == false) {
+            Gate::authorize('create-comment', Comment::class);
+        }
+        
+        // Define guest rules if user is not logged in.
+        if (!Auth::check()) {
+            $guest_rules = [
+                'guest_name' => 'required|string|max:255',
+                'guest_email' => 'required|string|email|max:255',
+            ];
+        }
+        
+        // Merge guest rules, if any, with normal validation rules.
+        Validator::make($request->all(), array_merge($guest_rules ?? [], [
+            'commentable_type' => 'required|string',
+            'commentable_id' => 'required|string|min:1',
+            'message' => 'required|string',
+          
+            'advantages'=>'json',
+            'disadvantages'=>'json'
+        ]))->validate();
+        
+        $model = $request->commentable_type::findOrFail($request->commentable_id);
+        
+        $commentClass = Config::get('comments.model');
+        $comment = new $commentClass;
+        
+        if (!Auth::check()) {
+            $comment->guest_name = $request->guest_name;
+            $comment->guest_email = $request->guest_email;
+        } else {
+            $comment->commenter()->associate(Auth::user());
+        }
+        
+        $comment->commentable()->associate($model);
+        $comment->comment = $request->message;
+        $comment->advantages = $request->advantages;
+        $comment->disadvantages = $request->disadvantages;
+   
+        $comment->approved = !Config::get('comments.approval_required');
+        $comment->save();
+        
+        return response(['comment_id'=> $comment->getKey()]);
     }
     
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comment $comment)
-    {
-        //
-    }
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
+     * Updates the message of the comment.
      */
     public function update(Request $request, Comment $comment)
     {
-        $validated=$request->validate([
-            'accepted'=> 'required|boolean'
+        Gate::authorize('edit-comment', $comment);
+        
+        Validator::make($request->all(), [
+            'status' =>'requierd|boolean',
+        ])->validate();
+        
+        $comment->update([
+            'status' => $request->status
         ]);
         
-        $comment->accepted=$validated['accepted'];
-        $comment->save();
-        
-        return response([
-            'data'=>$comment
-            ],200);
+        return response(['comment_id'=> $comment->getKey()]);
     }
     
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
+     * Deletes a comment.
      */
     public function destroy(Comment $comment)
     {
-        $comment->delete();
+        Gate::authorize('delete-comment', $comment);
+        
+        if (Config::get('comments.soft_deletes') == true) {
+            $comment->delete();
+        }
+        else {
+            $comment->forceDelete();
+        }
+        
         return response(200);
+    }
+    
+    /**
+     * Creates a reply "comment" to a comment.
+     */
+    public function reply(Request $request, Comment $comment)
+    {
+        Gate::authorize('reply-to-comment', $comment);
+        
+        Validator::make($request->all(), [
+            'message' => 'required|string',
+            'advantages'=>'json',
+            'disadvantages'=>'json'
+            
+        ])->validate();
+        
+        $commentClass = Config::get('comments.model');
+        $reply = new $commentClass;
+        $reply->commenter()->associate(Auth::user());
+        $reply->commentable()->associate($comment->commentable);
+        $reply->parent()->associate($comment);
+        
+        $reply->comment = $request->message;
+        $comment->advantages = $request->advantages;
+        $comment->disadvantages = $request->disadvantages;
+        
+        $reply->approved = !Config::get('comments.approval_required');
+        $reply->save();
+        
+        return response(['comment_id'=> $comment->getKey()]);
     }
 }
